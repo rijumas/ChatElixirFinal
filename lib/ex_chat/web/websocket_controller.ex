765610de6ae17dc0e638d2a02cd239e0964debde
angel452 <angelloayzah2016@gmail.com>
2023-12-11 19:03:16 -0500

    Storage Message V2

--- lib/ex_chat/web/websocket_controller.ex
+++ lib/ex_chat/web/websocket_controller.ex
@@ -40,44 +40,25 @@ defmodule ExChat.Web.WebSocketController do
 
   defp handle(%{"command" => "join", "room" => room}, session_id) do
 
-    # Envía los mensajes almacenados al usuario
-    Logger.info("\n Join - #{session_id} user \n")
-
-    # Al unirse a una sala, obtén los mensaes existentes
-    messages = ExChat.MessageStore.get_messages(room)
-
-    Logger.info("\n ------------- INITIAL --------------------")
-    Logger.info("\n Message: #{inspect(messages)} \n")
-    #Logger.info("\n rest: #{rest} \n")
-    Logger.info("\n room: #{room} \n")
-    Logger.info("\n session_id: #{session_id} \n")
-    Logger.info("\n------------------------------------ \n")
-
-    Logger.info("\n !!! Fin del envio de mensajes almacenados with #{session_id} user !!! \n")
-
+    # Continúa con el resto del manejo del comando
     case JoinChatRoom.on(room, session_id) do
       :ok ->
+        # Envía los mensajes almacenados al usuario
+        # send_stored_messages(messages,room, session_id)
         {:ok, session_id}
       {:error, message} ->
         {:reply, {:text, to_json(%{error: message})}, session_id}
     end
-
-    #send_stored_messages(messages, room, session_id)
-
   end
 
   defp handle(command = %{"command" => "join"}, session_id) do
-    Logger.info("\n Join 2 \n")
     handle(Map.put(command, "room", "default"), session_id)
   end
 
+
   defp handle(%{"room" => room, "message" => message}, session_id) do
 
-    Logger.info("\n SendMessage \n")
-
-    # Continúa con el resto del manejo del mensaje
-    #IO.inspect(message, label: "Mensaje que llega al WebSocket")
-    #IO.inspect(session_id, label: "Este es el usuario")
+    #Logger.info("\n SendMessage \n")
 
     case SendMessageToChatRoom.on(message, room, session_id) do
       {:error, message} ->
@@ -85,6 +66,7 @@ defmodule ExChat.Web.WebSocketController do
       :ok ->
         # Almacenar el mensaje en el historial de la sala
         ExChat.MessageStore.add_message(room, session_id, message)
+
         # Imprimir el mensaje almacenado
         ExChat.MessageStore.print_messages(room)
 
@@ -123,15 +105,19 @@ defmodule ExChat.Web.WebSocketController do
   defp send_stored_messages([], _room, _session_id), do: :ok
   defp send_stored_messages([message | rest], room, session_id) do
 
-    Logger.info("\n ------------- FINAL -------------------- \n")
-    Logger.info("\n Message: #{inspect(message)} \n")
-    Logger.info("\n Rest: #{inspect(rest)} \n")
-    Logger.info("\n room: #{room} \n")
-    Logger.info("\n session_id: #{session_id} \n")
-    Logger.info("\n ------------------------------------ \n")
-
     # Envia un mensaje almacenado al usuario
-    SendMessageToChatRoom.on(message, room, session_id)
+    case SendMessageToChatRoom.on(message, room, session_id) do
+      {:error, message} ->
+        {:reply, {:text, to_json(%{error: message})}, session_id}
+      :ok ->
+        # Almacenar el mensaje en el historial de la sala
+        ExChat.MessageStore.add_message(room, session_id, message)
+
+        # Imprimir el mensaje almacenado
+        ExChat.MessageStore.print_messages(room)
+
+        {:ok, session_id}
+    end
 
     # Llama recursivamente para enviar el siguiente mensaje
     send_stored_messages(rest, room, session_id)
